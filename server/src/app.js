cab7f82fc3ed5486487d947c95dec4ea35d1e731
RizakSingh <srizak95@gmail.com>
2026-02-03 12:25:33 +0530

    backend files created and database connected

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
-
+import mongoose from "mongoose";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-  res.json({ status: "Backend running" });
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("Mongo error", err));
 
 export default app;
