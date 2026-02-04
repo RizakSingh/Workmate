6381f73f6999fd91e30b24a6177448aa94325b3a
RizakSingh <srizak95@gmail.com>
2026-02-04 18:21:31 +0530

    login page done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import { useState } from "react";
+import api from "../api/axios";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
-export default Login
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await api.post("/auth/login", {
+        email,
+        password,
+      });
+      login(res.data);
+      navigate("/dashboard");
+    } catch (err) {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="p-6 space-y-4">
+      <input
+        placeholder="Email"
+        className="border p-2 w-full"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="border p-2 w-full"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className="bg-black text-white px-4 py-2">
+        Login
+      </button>
+    </form>
+  );
+}
+export default Login;
